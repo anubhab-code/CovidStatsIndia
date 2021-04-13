38c93a01306f9b5ff849e5c7062c305a69607520
Anubhab Swain <anubhabswain69@gmail.com>
2021-04-13 18:44:41 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+import profile from "../profileAvatar.svg";
+
+const Footer = () => {
+    return ( <
+        footer >
+        <
+        div className = "left-info" >
+        <
+        div className = "profile" >
+        <
+        img src = { profile }
+        alt = "profile"
+        height = "40px" / >
+        <
+        /div> <
+        div className = "text_" >
+        <
+        p > Created By < /p> <
+        span >
+        <
+        p > Anubhab Swain < /p> <
+        /span> <
+        /div> <
+        /div> <
+        div className = "right-info" >
+        <
+        a href = "https://github.com/anubhab-code" >
+        <
+        i className = "fa fa-github fa-2x" > < /i> <
+        /a> <
+        a href = "https://www.linkedin.com/in/anubhabcodes/" >
+        <
+        i className = "fab fa-linkedin fa-2x" > < /i> <
+        /a> <
+        a href = "https://www.instagram.com/anubhab.exe/" >
+        <
+        i className = "fa fa-instagram fa-2x" > < /i> <
+        /a> <
+        /div> <
+        /footer>
+    );
+};
+
+export default Footer;
