3811666517a3bdce20a1b29ef39d3d37bd4f1c50
Anubhab Swain <anubhabswain69@gmail.com>
2021-04-14 01:46:25 +0530

    Updates

---
+++ precache-manifest.9ddfd5c5411eb8bba93b3135b1420888.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aad18d15c70cfdb84bc5ca4b5dd2b112",
+    "url": "/covidstatsindia/index.html"
+  },
+  {
+    "revision": "684f1a3c26e68f39949d",
+    "url": "/covidstatsindia/static/css/main.6d1528a0.chunk.css"
+  },
+  {
+    "revision": "ebf667adca7439214e8e",
+    "url": "/covidstatsindia/static/js/2.79679491.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/covidstatsindia/static/js/2.79679491.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "684f1a3c26e68f39949d",
+    "url": "/covidstatsindia/static/js/main.7c1d79ea.chunk.js"
+  },
+  {
+    "revision": "ac90b8317e4ba54a2e2f",
+    "url": "/covidstatsindia/static/js/runtime-main.f5f7e435.js"
+  },
+  {
+    "revision": "9432ddaf699e4df5f361bd94dec8925c",
+    "url": "/covidstatsindia/static/media/first_graphic.9432ddaf.svg"
+  },
+  {
+    "revision": "71bb19a1a747b0e3818eae2f0a9428f5",
+    "url": "/covidstatsindia/static/media/profileAvatar.71bb19a1.jpg"
+  },
+  {
+    "revision": "6b55a6169ceac7ef3448475ccbf41d6e",
+    "url": "/covidstatsindia/static/media/second_graphic.6b55a616.svg"
+  }
+]);
